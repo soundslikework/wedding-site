f539f3f1a2402fdb7b34d576e455778bba0f581f
Adam Brady <soundslikework@gmail.com>
2019-01-20 15:31:57 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/wedding-site/precache-manifest.57764479d25a2b825f603a599adbd9ff.js"
+  "/wedding-site/precache-manifest.60142e7aef05d4e2631148e4b0b87782.js"
 );
 
 workbox.clientsClaim();
