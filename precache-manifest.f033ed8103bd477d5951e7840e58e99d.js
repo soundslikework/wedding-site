b8d2b99d14e2fe31ab947d1666f6b7c37f64b7bc
Adam Brady <soundslikework@gmail.com>
2019-01-20 12:06:34 -0500

    Updates

---
+++ precache-manifest.f033ed8103bd477d5951e7840e58e99d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e8bd98e3178b2b4e83e2",
+    "url": "/wedding-site/static/js/runtime~main.e8bd98e3.js"
+  },
+  {
+    "revision": "798d558bc7b54008a0b3",
+    "url": "/wedding-site/static/js/main.798d558b.chunk.js"
+  },
+  {
+    "revision": "4ad756683673bcb3be35",
+    "url": "/wedding-site/static/js/1.4ad75668.chunk.js"
+  },
+  {
+    "revision": "798d558bc7b54008a0b3",
+    "url": "/wedding-site/static/css/main.16425802.chunk.css"
+  },
+  {
+    "revision": "f04da31c4cd4794ec920510e8b07cdff",
+    "url": "/wedding-site/index.html"
+  }
+];
